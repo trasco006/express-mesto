62b9c2ef1c397c898619bd246709b2b8d1df1ff7
Kirill Sychevnik <trasco006@gmail.com>
2021-02-11 23:37:14 +0300

    fixed cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,14 +5,12 @@ const getCards = (req, res) => {
     .then(cards => res.status(200).send(cards))
     .catch(err => res.status(500).send(err))
 }
-
 const addCard = (req, res) => {
   const {body} = req
   Card.create(body)
     .then(card => res.status(200).send(card))
     .catch(err => res.status(500).send(err))
 }
-
 const deleteCard = (req, res) => {
   Card.findOne({_id: req.params.cardId})
     .then(card => {
@@ -22,19 +20,19 @@ const deleteCard = (req, res) => {
     })
     .catch(res.status(500).send('Карточки с таким id не существует.'))
 }
-
-const likeCard = (req, res)=>{
+const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
-    { new: true },
+    {$addToSet: {likes: req.user._id}},
+    {new: true},
   )
 }
-
-const dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {$pull: {likes: req.user._id}},
+    {new: true},
+  )
+}
 
 module.exports = {getCards, addCard, deleteCard, likeCard, dislikeCard}
